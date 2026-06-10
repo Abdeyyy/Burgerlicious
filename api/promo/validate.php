be1d33327cafdd398a1660777922cceadd1e72e6
ASN-Lab <asn.labs1210@gmail.com>
2026-06-10 19:31:23 +0700

    Fix: Mobile access API

--- api/promo/validate.php
+++ api/promo/validate.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 header('Content-Type: application/json');
 require_once '../../config/db.php';
 require_once '../../auth/auth_helper.php';
